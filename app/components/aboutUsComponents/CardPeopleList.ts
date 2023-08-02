b3c448dfb074c9d17cd79b2b6f497c551686fe27
ProstoPonchik <vlad.vislovukh@gmail.com>
2023-08-02 13:12:36 +0200

    some fix

--- app/components/aboutUsComponents/CardPeopleList.ts
+++ app/components/aboutUsComponents/CardPeopleList.ts
@@ -2,42 +2,42 @@ export const cardPeopleList = [
   {
     name: "Захар",
     position: "Основатель",
-    image: "/../public/pictures/persons/Захар.jpg",
+    image: "/public/pictures/persons/Захар.jpg",
     description:
       "Захар задаёт вектор развития агентства, обучает команду и делится с ней своим предпринимательским опытом",
   },
   {
     name: "Дмитрий",
     position: "СТО",
-    image: "/../public/pictures/persons/Дмитрий.png",
+    image: "/public/pictures/persons/Дмитрий.png",
     description:
       "Управляет продуктовой командой и обучает искусственный интеллект: делает его умнее и эффективнее",
   },
   {
     name: "Никита",
     position: "Руководитель по работе с клиентами",
-    image: "/../public/pictures/persons/Никита.jpg",
+    image: "/public/pictures/persons/Никита.jpg",
     description:
       "Задача Никиты — сделать так, чтобы каждый клиент NOAH на 100% закрывал свои бизнес-цели и оставался доволен работой агентства",
   },
   {
     name: "Сакен",
     position: "Проектный менеджер",
-    image: "/../public/pictures/persons/Сакен.png",
+    image: "/public/pictures/persons/Сакен.png",
     description:
       "Управляет командой, которая работает над стратегиями продвижения клиентов: следит за сроками и качеством исполнения",
   },
   {
     name: "Александр",
     position: "Инженер технической поддержки",
-    image: "/../public/pictures/persons/Александр.jpg",
+    image: "/public/pictures/persons/Александр.jpg",
     description:
       "Обеспечивает поддержку клиентов, разрешая технические вопросы, предоставляя решения и улучшая опыт пользователей.",
   },
   {
     name: "Анастасия",
     position: "Главный дизайнер",
-    image: "/../public/pictures/persons/Анастасия.png",
+    image: "/public/pictures/persons/Анастасия.png",
     description:
       "Занимается разработкой айдентики для проектов: придумывает визуальное оформление профилей и готовит креативы постов",
   },
--- app/components/buttons/buttonSendCV.tsx
+++ app/components/buttons/buttonSendCV.tsx
@@ -1,7 +1,13 @@
 const ButtonSendCV = () => {
 	return (
-		<div className="w-[145px] h-[60px] px-[14.33px] py-[9.27px] bg-teal-500 rounded-[5.06px] justify-center items-center gap-[8.43px] inline-flex">
-			<div className="text-white text-base font-bold leading-7">Button</div>
+		<div
+			className="w-[232px] h-[60px] px-[14.33px] py-[9.27px] bg-teal-500 rounded-[5.06px] justify-center items-center gap-[8.43px] inline-flex cursor-pointer
+		hover:bg-[#14929A] transition duration-200 ease-in-out
+		"
+		>
+			<div className="text-white text-base font-bold leading-7">
+				Отправить резюме
+			</div>
 		</div>
 	);
 };
